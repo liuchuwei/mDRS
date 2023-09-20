4d7235a2971bb8ccaabb4c8172bbbda0f1cd8940
Liuchuwei <liuchw3@mail2.sysu.edu.cn>
2023-09-20 15:13:26 +0800

    update

---
+++ 04.resquiggle.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import sys
+import os
+
+from tookit import Tookits
+
+# args define
+parser = argparse.ArgumentParser(description='Basecalling')
+parser.add_argument('-f', '--fast5', required=True, help="directory of fast5 files")
+parser.add_argument('-fq', '--fastq', required=True, help="directory of merge fastq files")
+parser.add_argument('-r', '--reference', required=True, help="directory of reference files")
+parser.add_argument('-o', '--output', required=True, help="output directory")
+args = parser.parse_args(sys.argv[1:])
+global FLAGS
+FLAGS = args
+
+from tookit import Tookits
+tools = Tookits()
+
+# # Convert merged single big fast5 into small size fast5 file
+fl = os.listdir(FLAGS.fast5)[0]
+fsize = os.path.getsize(os.path.join(FLAGS.fast5,fl))
+fsize = fsize/float(1024*1024)
+
+# create output directory
+single_out = FLAGS.output + "/single"
+
+dirs = single_out.split("/")
+dirs_list = []
+for i in range(len(dirs)):
+    dirs_list.append("/".join(dirs[0:i + 1]))
+
+for item in dirs_list:
+    if not os.path.exists(item):
+        os.mkdir(item)
+
+
+if fsize > 10:
+
+    cmd = "%s -i %s -s %s -t 40 --recursive" % (tools.multi_to_single_fast5, FLAGS.fast5, single_out)
+    # os.system(cmd)
+
+    FLAGS.fast5 = single_out
+
+    # annotate_raw_with_fastqs
+    cmd = "%s preprocess annotate_raw_with_fastqs \
+    --fast5-basedir %s \
+    --fastq-filenames %s \
+    --overwrite \
+    --processes 8" % (tools.tombo, FLAGS.output, FLAGS.fastq)
+    os.system(cmd)
+
+else:
+    single_out = FLAGS.output + "/single"
+    # cmd = "cp %s/*.fast5 %s" % (FLAGS.fast5, single_out)
+    cmd = "find %s -name  '*.fast5' | xargs -i cp {} %s" % (FLAGS.fast5, single_out)
+    os.system(cmd)
+    FLAGS.fast5 = single_out
+
+# resquiggle raw signals
+cmd = "%s resquiggle %s %s \
+--rna \
+--corrected-group RawGenomeCorrected_000 \
+--basecall-group Basecall_1D_000 \
+--overwrite \
+--processes 16 \
+--fit-global-scale \
+--include-event-stdev" % (tools.tombo, FLAGS.fast5, FLAGS.reference)
+os.system(cmd)
